784258f9e427a9684537c1f2fedc3d7fde969c9c
abdulmajid <majid@kebun.tech>
2023-08-30 06:17:26 +0700

    setup config for deploy server

--- servers/app.js
+++ servers/app.js
@@ -5,9 +5,6 @@ const app = express();
 const cors = require('cors')
 const PORT = process.env.PORT || 4000
 const routes = require('./routes')
-// const erroHandler = require('./middlewares/errorHandlers')
-// const { register, login, authenticate } = require('./controllers/authController')
-// const MovieController = require('./controllers/movieController')
 
 sequelize.authenticate()
   .then(() => {
@@ -24,20 +21,6 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(routes)
 
-// app.get('/', (req, res) => {
-//   res.json({ message: "HELLO WORLD HOME" })
-// })
-
-// app.post('/register', register)
-// app.post('/login', login)
-
-// app.use(authenticate)
-// app.get('/protected', (req, res) => {
-//   res.json({ message: "HELLO WORLD"})
-// })
-// app.get('/movies', MovieController.showListMovies)
-
 app.listen(PORT, (_) => {
 	console.log(`server listening on port ${PORT}`)
 })
-
